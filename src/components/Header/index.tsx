2bf10392c84123fd95e77fc4a01d6d7a17cbc21a
Caio Augusto <caioamfr@gmail.com>
2021-06-17 14:09:51 -0300

    Modal pra criar nova transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,18 @@ import logo from "assets/logo.svg";
 
 import * as S from "./styles";
 
-export const Header = () => {
+export type HeaderProps = {
+  handleOpenNewTransactionModal: () => void;
+};
+
+export const Header = ({ handleOpenNewTransactionModal }: HeaderProps) => {
   return (
     <S.Header>
       <S.Content>
         <img src={logo} alt="Logo da dtmoney" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={handleOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </S.Content>
     </S.Header>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,17 @@
+import Modal from "react-modal";
+
+export type NewTransactionModalProps = {
+  isOpen: boolean;
+  onRequestClose: () => void;
+};
+
+export function NewTransactionModal({
+  isOpen,
+  onRequestClose,
+}: NewTransactionModalProps) {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+      <h2>Cadastrar transação</h2>
+    </Modal>
+  );
+}
